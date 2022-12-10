f7e84ea46e0c652c8999f8576e2d01ce2a58f5d4
Jessada Taengsuwan <65070041@kmitl.ac.th>
2022-12-11 01:12:01 +0700

    Update frontend

--- views.py
+++ views.py
@@ -2,16 +2,8 @@ from flask import Blueprint, render_template, request, redirect, url_for
 
 views = Blueprint(__name__, "views")
 
-@views.route("/")
+@views.route('/')
 def index():
-    return render_template("index.html", name = "")
-
-@views.route("/profile")
-def profile():
-    args = request.args
-    name = args.get('name')
-    return render_template("index.html", name=name)
-
-@views.route("go_home")
-def go_index():
-    return redirect(url_for("views.index"))
+    """home page"""
+    weight = request.form["fweight"]
+    return render_template("index.html")
