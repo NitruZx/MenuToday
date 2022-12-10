ef5b6223909935358f154b68ab0e1bb33e811a03
Jessada Taengsuwan <65070041@kmitl.ac.th>
2022-12-11 02:11:27 +0700

    Update user input

--- views.py
+++ views.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-
+from MenuToday2 import *
 views = Blueprint(__name__, "views")
 
-@views.route('/')
+@views.route('/', methods=["POST", "GET"])
 def index():
     """home page"""
-    weight = request.form["fweight"]
-    return render_template("index.html")
+    ans = ''
+    if request.method == "POST" and "fweight" in request.form and "fhight" in request.form:
+        weight = float(request.form.get('weight'))
+        hight = float(request.form.get('hight'))
+        ans = bmi(weight, hight)
+    return render_template("index.html", bmi=ans)
