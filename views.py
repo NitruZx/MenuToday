d4cca203baa85c6269835faa9c9a6d43ce39c0e5
Jessada Taengsuwan <65070041@kmitl.ac.th>
2022-12-13 01:48:19 +0700

    Update random menu

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from MenuToday3 import bmi, bmr, check_u_bmi, calpermeal, find_menu
+from MenuToday4 import bmi, bmr, check_u_bmi, calpermeal, find_menu, outmenu
 views = Blueprint(__name__, "views")
 
 tmp = ["", "", "", ""]
@@ -23,12 +23,25 @@ def index():
 @views.route('/random', methods=["POST", "GET"])
 def meal():
     """random meal page"""
+    final = 0
     if request.method == "GET":
+        tmp = ["", "", ""]
         return render_template("random.html")
     if request.method == "POST":
         want1 = request.form.get('select1')
         want2 = request.form.get('select2')
         want3 = request.form.get('select3')
+        if request.form.get('meal') is None:
+            meal_day = 0
+        else:
+            meal_day = int(request.form.get('meal'))
         
-        clean = [i for i in [want1, want2, want3] if not i is None]
-    return render_template("random.html", tmp=tmp)
+        if request.form.get('cal') is None:
+            cal_day = 0
+        else:
+            cal_day = int(request.form.get('cal'))
+        menu = [i for i in [want1, want2, want3] if not i is None]
+        final = outmenu(meal_day, menu, cal_day)
+        tmp = [meal_day, cal_day, menu]
+        print(final[0])
+    return render_template("random.html", menu_out = final, tmp=tmp)
